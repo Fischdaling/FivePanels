5370d610b4d72f283c2fe8c287e0c89a851af26b
Fischling <gabrielrokyta@gmail.com>
2024-05-03 17:17:00 +0200

    Test merge

--- src/main/java/org/theShire/Main.java
+++ src/main/java/org/theShire/Main.java
@@ -3,5 +3,6 @@ package org.theShire;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Samuel shall not Change Me");
     }
 }
