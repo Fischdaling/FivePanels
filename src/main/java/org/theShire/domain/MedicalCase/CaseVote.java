b948052c8a033d926e8d73ecd3f01eb5b05dbc4b
Haue04 <44940821+Haue04@users.noreply.github.com>
2024-05-13 00:00:03 +0200

    CaseVote usw

--- src/main/java/org/theShire/domain/MedicalCase/CaseVote.java
+++ src/main/java/org/theShire/domain/MedicalCase/CaseVote.java
@@ -1,4 +1,14 @@
 package org.theShire.domain.MedicalCase;
 
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.UUID;
+
 public class CaseVote {
+    private LinkedHashSet<Answers> answers;
+    private HashMap<UUID, Set<Vote>> votes;
+    private int maxAnswers;
+    private double prozentCount;
+    private final double maxProzentCount = 100.0;
 }
