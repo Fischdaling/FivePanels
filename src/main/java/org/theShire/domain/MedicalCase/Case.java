5d32ca673fc6e2598191435c148e2a3ac5fa56d1
Haue04 <44940821+Haue04@users.noreply.github.com>
2024-05-12 23:44:15 +0200

    Case Attributes

--- src/main/java/org/theShire/domain/MedicalCase/Case.java
+++ src/main/java/org/theShire/domain/MedicalCase/Case.java
@@ -10,7 +10,7 @@ public class Case extends BaseEntity {
     private String title;
     private List<Content> content;
     private Set<Specialization> specialization;
-    private int viewcunt;
+    private int viewcount;
     private UUID ownerid;
     private Set<UUID> members;
     private int likeCount;
@@ -18,7 +18,90 @@ public class Case extends BaseEntity {
     private Set<Category> category;
     private CaseVote caseVote;
 
+
     public Case(Instant createdAt) {
         super(Instant.now());
     }
+
+    //getter and setter----- //TODO ASSERT
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Content> getContent() {
+        return content;
+    }
+
+    public void setContent(List<Content> content) {
+        this.content = content;
+    }
+
+    public Set<Specialization> getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(Set<Specialization> specialization) {
+        this.specialization = specialization;
+    }
+
+    public int getViewcount() {
+        return viewcount;
+    }
+
+    public void setViewcount(int Viewcount) {
+        this.viewcount = Viewcount;
+    }
+
+    public UUID getOwnerid() {
+        return ownerid;
+    }
+
+    public void setOwnerid(UUID ownerid) {
+        this.ownerid = ownerid;
+    }
+
+    public Set<UUID> getMembers() {
+        return members;
+    }
+
+    public void setMembers(Set<UUID> members) {
+        this.members = members;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public Set<UUID> getUserLiked() {
+        return userLiked;
+    }
+
+    public void setUserLiked(Set<UUID> userLiked) {
+        this.userLiked = userLiked;
+    }
+
+    public Set<Category> getCategory() {
+        return category;
+    }
+
+    public void setCategory(Set<Category> category) {
+        this.category = category;
+    }
+
+    public CaseVote getCaseVote() {
+        return caseVote;
+    }
+
+    public void setCaseVote(CaseVote caseVote) {
+        this.caseVote = caseVote;
+    }
+    //------------------
 }
