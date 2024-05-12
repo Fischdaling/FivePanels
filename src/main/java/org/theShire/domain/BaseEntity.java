38e080aa95e3acdc13502ba855cd03fd8dae20a0
Fischling <gabrielrokyta@gmail.com>
2024-05-12 23:16:47 +0200

    BaseEntity

--- src/main/java/org/theShire/domain/BaseEntity.java
+++ src/main/java/org/theShire/domain/BaseEntity.java
@@ -1,8 +1,36 @@
 package org.theShire.domain;
 
+import org.theShire.foundation.DomainAssertion;
+import java.time.Instant;
+import java.util.UUID;
+
+
 public class BaseEntity {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Samuel shall not Change Me");
+    private UUID entityId;
+    private Instant createdAt;
+    private Instant updatedAt;
+
+    public BaseEntity(Instant createdAt) {
+        this.entityId = UUID.randomUUID();
+        this.createdAt = Instant.now();
+        this.updatedAt = Instant.now();
     }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public UUID getEntityId() {
+        return entityId;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        DomainAssertion.isBeforeTime(updatedAt,getCreatedAt(),"updatedAt");
+        this.updatedAt = updatedAt;
+    }
+
 }
