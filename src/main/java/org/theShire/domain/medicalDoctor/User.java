2a4e083b27113798aade4bca6900fead1c090b0c
Fischling <gabrielrokyta@gmail.com>
2024-05-17 21:02:44 +0200

    extract main to presentation

--- src/main/java/org/theShire/domain/medicalDoctor/User.java
+++ src/main/java/org/theShire/domain/medicalDoctor/User.java
@@ -73,8 +73,12 @@ public class User extends BaseEntity {
     }
 
 
-    public Set<Chat> getChats() {
-        return chats;
+    public Chat getChatByID(UUID id) {
+        return chats.stream().filter(chat -> chat.getEntityId().equals(id)).findFirst().orElse(null);
+    }
+
+    public List<Chat> getChatByUser(User name) {
+        return chats.stream().filter(chat -> chat.getPeople().contains(name)).toList();
     }
 
 
@@ -110,63 +114,11 @@ public class User extends BaseEntity {
         sb.append("score=").append(score).append(System.lineSeparator());
         sb.append("contacts=").append(contacts).append(System.lineSeparator());
         sb.append("chats=").append(chats).append(System.lineSeparator());
-        sb.append("ownedCases=").append(ownedCases).append(System.lineSeparator());
+//        sb.append("ownedCases=").append(ownedCases).append(System.lineSeparator());
         sb.append("memberOfCase=").append(memberOfCase).append(System.lineSeparator());
         sb.append('}');
         return sb.toString();
     }
 
-    public static void main(String[] args) {
-        //TODO Relation && builder
-        //Creating ProfilePic
-        Media media = new Media(2000,1500,"I am a Picture", "2000x1500");
-        //Create Profile
-        UserProfile profile = new UserProfile(new Language("German"),
-                new Location("Gondor"),media,new Name("Boromir"),
-                new Name("Aragorn"),new EducationalTitle("Dr."),
-                new EducationalTitle("arathorn"));
-
-
-        //Create User
-        User user = new User(new Password("Spengergasse"),
-                new Email("Boromir@gamil.com"),profile);
-
-
-        //Create ProfilePic
-        Media media1 = new Media(1500,100,"I am a Father", "1500x100");
-        //Create Profile
-        UserProfile profile1 = new UserProfile(new Language("German"),
-                new Location("Gondor"),media1,new Name("Aarathorn"),
-                new Name("Aragorn"),new EducationalTitle("Dr."),
-                new EducationalTitle("Arathorn"),new EducationalTitle("mag"));
-        //Create USer
-        User user1 = new User(new Password("Spengergasse123"),
-                new Email("Arathorn@gamil.com"),profile1);
-
-        //init Content
-        List<Content> contents = new ArrayList<>();
-        //add texts
-        contents.add(new Content(new ContentText("My First Text")));
-        contents.add(new Content(new ContentText("My Second Text")));
-        //add Media
-        contents.add(new Content(new Media(200,100,"My First Media", "200x100")));
-
-        //Create a Case with user 1 as member and user as owner
-        user.createCase("my First Case", contents,user1);
-
-        // TODO NO GOOD Solution
-        UserRelationShip relationShip = new UserRelationShip(user,user1);
-        relationShip.acceptRequest(user.getEntityId(),user1.getEntityId());
-
-        for (Chat chat : user.getChats()) {
-            chat.sendMessage(new Message(user.getEntityId(),new Content(new ContentText("Hi"))));
-        }
-        for (Chat chat : user1.getChats()) {
-            chat.sendMessage(new Message(user.getEntityId(),new Content(new ContentText("Yo"))));
-        }
-
-        System.out.println(user.chats);
-
-    }
 
 }
