74e4746dd7c7c8955fefae9e5e6c6164d16ee1a2
Haue04 <44940821+Haue04@users.noreply.github.com>
2024-06-03 02:42:36 +0200

    split the giant test method into 4 and iniatet a BeforeEach

--- src/test/java/org/theShire/domain/medicalCase/CaseVoteTest.java
+++ src/test/java/org/theShire/domain/medicalCase/CaseVoteTest.java
@@ -1,5 +1,6 @@
 package org.theShire.domain.medicalCase;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -10,40 +11,81 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CaseVoteTest {
+    LinkedHashSet<Answer> answerLul;
+    UUID userID;
+    Answer answer;
+    Answer answer1;
+    CaseVote caseVote;
+    HashMap<UUID, Set<Vote>> votesSafe;
 
-    @Test
-    void TestVoting_ShouldBeEqualToOtherVoteObjects_WhenDeterminedAsParameter() {
+    @BeforeEach
+    public void init() {
         //initialization of the LinkedHashSet filled with Answers
-        LinkedHashSet<Answer> answerLul = new LinkedHashSet<>();
-        UUID userID = UUID.randomUUID();
-        Answer answer = new Answer("Krebs");
-        Answer answer1 = new Answer("Ebola");
+        answerLul = new LinkedHashSet<>();
+        userID = UUID.randomUUID();
+        answer = new Answer("Krebs");
+        answer1 = new Answer("Ebola");
         answerLul.add(answer);
         answerLul.add(answer1);
+        caseVote = new CaseVote(answerLul);
+        votesSafe = new HashMap<>();
+    }
 
-        //Slowly starting with business by creating a CaseVote Instance
-        CaseVote caseVote = new CaseVote(answerLul);
-        HashMap<UUID, Set<Vote>> votesSafe = new HashMap<>();
 
+    @Test
+    void TestVoting_ShouldReturnTrue_WhenComparedToAnEqualObject() {
         //testing if the UUID stays the same after voting throughout saving the old values of the votes Hashmap
         votesSafe = caseVote.getVotes();
         caseVote.voting(userID, answer1, 10.0);
         assertEquals(votesSafe.values(), caseVote.getVotes().values());
 
         Vote vote1 = new Vote(answer1, 10.0);
-        Vote vote2 = new Vote(answer1, 5.0);
-        Vote vote3 = new Vote(answer, 10.0);
-        Vote vote4 = new Vote(answer, 5.0);
 
 
         assertEquals(caseVote.getVotes().get(userID).stream().findFirst().orElse(null).getAnswer(), vote1.getAnswer());
         assertEquals(caseVote.getVotes().get(userID).stream().findFirst().orElse(null).getPercent(), vote1.getPercent());
 
+    }
+
+    @Test
+    void TestVoting_ShouldReturnTrue_WhenComparedToASemiEqualObject() {
+        //testing if the UUID stays the same after voting throughout saving the old values of the votes Hashmap
+        votesSafe = caseVote.getVotes();
+        caseVote.voting(userID, answer1, 10.0);
+        assertEquals(votesSafe.values(), caseVote.getVotes().values());
+
+
+        Vote vote2 = new Vote(answer1, 5.0);
+
+
         assertEquals(caseVote.getVotes().get(userID).stream().findFirst().orElse(null).getAnswer(), vote2.getAnswer());
         assertNotEquals(caseVote.getVotes().get(userID).stream().findFirst().orElse(null).getPercent(), vote2.getPercent());
 
+    }
+
+    @Test
+    void TestVoting_ShouldReturnTrue_WhenComparedToASemiEqualObjectv2() {
+        //testing if the UUID stays the same after voting throughout saving the old values of the votes Hashmap
+        votesSafe = caseVote.getVotes();
+        caseVote.voting(userID, answer1, 10.0);
+        assertEquals(votesSafe.values(), caseVote.getVotes().values());
+
+
+        Vote vote3 = new Vote(answer, 10.0);
+
+
         assertNotEquals(caseVote.getVotes().get(userID).stream().findFirst().orElse(null).getAnswer(), vote3.getAnswer());
         assertEquals(caseVote.getVotes().get(userID).stream().findFirst().orElse(null).getPercent(), vote3.getPercent());
+    }
+
+    void TestVoting_ShouldReturnTrue_WhenComparedToANonEqualObject() {
+        //testing if the UUID stays the same after voting throughout saving the old values of the votes Hashmap
+        votesSafe = caseVote.getVotes();
+        caseVote.voting(userID, answer1, 10.0);
+        assertEquals(votesSafe.values(), caseVote.getVotes().values());
+
+        Vote vote4 = new Vote(answer, 5.0);
+
 
         assertNotEquals(caseVote.getVotes().get(userID).stream().findFirst().orElse(null).getAnswer(), vote4.getAnswer());
         assertNotEquals(caseVote.getVotes().get(userID).stream().findFirst().orElse(null).getPercent(), vote4.getPercent());
